7068717d2613847b6c93651f97aa569063672752
Eric D. Reichert <eric@reichertconsulting.com>
2015-11-07 23:32:18 -0500

    Figured out how to test the debug macro and added the appropriate tests.

--- src/logging.rs
+++ src/logging.rs
@@ -17,3 +17,35 @@ macro_rules! debug {
 
     ($debug:ident, $fmt:expr, $($arg:tt)*) => { write_log!(&mut ::std::io::stderr(), $debug, "DEBUG", $fmt, $($arg)*) };
 }
+
+#[cfg(test)]
+mod test {
+    use std::fmt::Write;
+
+    #[test]
+    fn debug_should_write_a_message_without_format_args_to_the_dest() {
+        let mut buff = String::new();
+
+        write_log!(&mut buff, true, "DEBUG", "This is a test without format args.", );
+
+        assert_eq!(buff, "DEBUG: This is a test without format args.\n");
+    }
+
+    #[test]
+    fn debug_should_write_a_message_with_one_format_arg_to_the_dest() {
+        let mut buff = String::new();
+
+        write_log!(&mut buff, true, "DEBUG", "This is a test with {} format arg.", "one");
+
+        assert_eq!(buff, "DEBUG: This is a test with one format arg.\n");
+    }
+
+    #[test]
+    fn debug_should_write_a_message_with_format_args_to_the_dest() {
+        let mut buff = String::new();
+
+        write_log!(&mut buff, true, "DEBUG", "This is a test with {} {}.", "format", "args");
+
+        assert_eq!(buff, "DEBUG: This is a test with format args.\n");
+    }
+}
