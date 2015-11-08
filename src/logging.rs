9bc31f00d3d0be34627ab1d6093a8aa57b61bfbf
Eric D. Reichert <eric@reichertconsulting.com>
2015-11-07 22:40:08 -0500

    Refactored the debug macro to make it testable.

--- src/logging.rs
+++ src/logging.rs
@@ -1,14 +1,19 @@
 #[macro_export]
-macro_rules! debug {
-    ($debug:ident, $fmt:expr, $($arg:tt)*) => {
+macro_rules! write_log {
+    ($dest:expr, $debug:ident, $level:expr, $fmt:expr, $($arg:tt)*) => {
         if $debug {
             use std::io::Write;
-            match writeln!(&mut ::std::io::stderr(), concat!("DEBUG: ", $fmt), $($arg)*) {
+            match writeln!($dest, concat!(concat!($level, ": "), $fmt), $($arg)*) {
                 Ok(_) => {},
                 Err(x) => panic!("Unable to write to stderr: {}", x),
             }
         }
     };
+}
 
-    ($debug:ident, $msg:expr) => { debug!($debug, $msg, ) }
+#[macro_export]
+macro_rules! debug {
+    ($debug:ident, $msg:expr) => { write_log!(&mut ::std::io::stderr(), $debug, "DEBUG", $msg, ) };
+
+    ($debug:ident, $fmt:expr, $($arg:tt)*) => { write_log!(&mut ::std::io::stderr(), $debug, "DEBUG", $fmt, $($arg)*) };
 }
