b8ca11ad9d30ea5a61fe320bd4c62ba9152b74c9
Eric D. Reichert <eric@reichertconsulting.com>
2017-02-02 21:40:50 -0500

    Removed the file_list method as it really didn't belong in ELP. That's an implementation detail that should be left to the ELP clients.

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,7 @@ extern crate log;
 #[macro_use]
 extern crate lazy_static;
 
-use std::path::Path;
-use self::walkdir::{DirEntry, WalkDir, WalkDirIterator};
+use self::walkdir::DirEntry;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
@@ -110,28 +109,6 @@ impl Error for ELBRecordParsingError {
             ELBRecordParsingError::CouldNotOpenFile { .. } => "failed to open file",
         }
     }
-}
-
-/// A utility method for retrieving all of the paths to ELB log files in a directory.
-///
-/// If the user uses the [AWS S3 sync tool](http://docs.aws.amazon.com/cli/latest/reference/s3/sync.html)
-/// to download their AWS ELB logs to a local disk the files will be in a very specific directory
-/// hierarchy.  This utility will read the paths of the files, recursively searching a root
-/// specified by the user, and append the paths to the `Vec<DirEntry>`, also provided by the user.
-///
-/// dir: The directory from which the paths of the ELB log files will be procured.
-///
-/// filenames: A Vec<DirEntry> to which the paths of the ELB log files will be written.
-pub fn file_list(dir: &Path, filenames: &mut Vec<DirEntry>) -> Result<usize, walkdir::Error> {
-    let dir_entries = WalkDir::new(dir)
-        .min_depth(1)
-        .into_iter()
-        .filter_entry(|e| e.file_name().to_str().map(|s| s.ends_with(".log")).unwrap_or(false));
-    for entry in dir_entries {
-        let entry = entry?;
-        filenames.push(entry);
-    }
-    Ok(filenames.len())
 }
 
 /// Attempt to parse every ELB record in every file in `filenames` and pass the results to the
