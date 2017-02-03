28c48949149ea490d0f2591898665a2ff5472eba
Eric D. Reichert <eric@reichertconsulting.com>
2017-02-02 21:59:05 -0500

    Removed more of the file processing code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
-extern crate walkdir;
 extern crate chrono;
 #[macro_use]
 extern crate log;
 #[macro_use]
 extern crate lazy_static;
 
-use self::walkdir::DirEntry;
-use std::fs::File;
-use std::io::BufReader;
-use std::io::BufRead;
 use self::chrono::{DateTime, UTC};
 use std::error::Error;
 use std::str::FromStr;
@@ -73,13 +68,6 @@ pub enum ELBRecordParsingError {
         field_name: ELBRecordField,
         description: String,
     },
-    /// Returned if a line in an ELB file cannot be read.  Most likely the result of a bad file on
-    /// disk.
-    LineReadError,
-    /// Returned if an ELB file cannot be opened.  Most likely the result of a bad file on disk.
-    CouldNotOpenFile {
-        path: String,
-    },
 }
 
 impl Display for ELBRecordParsingError {
@@ -92,10 +80,6 @@ impl Display for ELBRecordParsingError {
                        field_name,
                        description)
             }
-            ELBRecordParsingError::LineReadError => write!(f, "Unable to read a line."),
-            ELBRecordParsingError::CouldNotOpenFile { ref path } => {
-                write!(f, "Unable to open file {}.", path)
-            }
         }
     }
 }
@@ -105,71 +89,8 @@ impl Error for ELBRecordParsingError {
         match *self {
             ELBRecordParsingError::MalformedRecord => "malformed record",
             ELBRecordParsingError::ParsingError { .. } => "field parsing failed",
-            ELBRecordParsingError::LineReadError => "failed to read line",
-            ELBRecordParsingError::CouldNotOpenFile { .. } => "failed to open file",
         }
     }
-}
-
-/// Attempt to parse every ELB record in every file in `filenames` and pass the results to the
-/// record_handler.
-///
-/// Each file will be opened and each line, which should represent a ELB record, will be passed
-/// through the parser.
-///
-/// # Failures
-///
-/// All failures including file access, file read, and parsing failures are passed to the
-/// record_handler as a `ParsingErrors`.
-pub fn process_files<H>(filenames: &[DirEntry], record_handler: &mut H) -> usize
-    where H: FnMut(ParsingResult) -> ()
-{
-
-    let mut total_record_count = 0;
-    for filename in filenames {
-        debug!("Processing file {}.", filename.path().display());
-        match File::open(filename.path()) {
-            Ok(file) => {
-                let file_record_count = handle_file(file, record_handler);
-                debug!("Found {} records in file {}.",
-                       file_record_count,
-                       filename.path().display());
-                total_record_count += file_record_count;
-            }
-
-            Err(_) => {
-                record_handler(Err(ParsingErrors {
-                    record: "",
-                    errors: vec![ELBRecordParsingError::CouldNotOpenFile {
-                                     path: format!("{}", filename.path().display()),
-                                 }],
-                }))
-            }
-        }
-    }
-
-    total_record_count
-}
-
-fn handle_file<H>(file: File, record_handler: &mut H) -> usize
-    where H: FnMut(ParsingResult) -> ()
-{
-    let mut file_record_count = 0;
-    for possible_record in BufReader::new(&file).lines() {
-        file_record_count += 1;
-        match possible_record {
-            Ok(record) => record_handler(parse_record(&record)),
-
-            Err(_) => {
-                record_handler(Err(ParsingErrors {
-                    record: "",
-                    errors: vec![ELBRecordParsingError::LineReadError],
-                }))
-            }
-        }
-    }
-
-    file_record_count
 }
 
 /// Attempt to parse a single string into an ELB record.
